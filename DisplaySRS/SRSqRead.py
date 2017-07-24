56310d7282f72bc707c2c67bdb5301e5d75280b2
Schwana <schwana@gmail.com>
2017-07-24 22:09:05 +0100

    SRSqRead.py has basic graph on. Closes after time to allow main QuadCTRL to continue. Fine as placeholder.

--- DisplaySRS/SRSqRead.py
+++ DisplaySRS/SRSqRead.py
@@ -1,5 +1,8 @@
 import time
 import sys
+import pyqtgraph as pg
+import numpy as np
+
 from time import strftime
 
 from PyQt4 import QtCore, QtGui
@@ -19,19 +22,38 @@ class SRSForm(QtGui.QMainWindow):
       self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
 
       #Initialise Clock
-      self.timer = QtCore.QTimer(self)
-      self.timer.timeout.connect(self.Time)
-      self.timer.start(1000)
+ #     self.timer = QtCore.QTimer(self)
+  #    self.timer.timeout.connect(self.Time)
+   #   self.timer.start(1000)
 
       self.tester = QtCore.QTimer(self)
       self.tester.timeout.connect(self.Test)
       self.tester.start()
+      
+      self.resize(800,800)
+      view = pg.GraphicsLayoutWidget()  ## GraphicsView with GraphicsLayout inserted by default
+      self.setCentralWidget(view)
 
-    def Time(self):
-        self.ui.lcdTime.display(strftime("%H"+":"+"%M"+":"+"%S"))
+
+      w1 = view.addPlot()
+
+      
+      n = 100
+      s1 = pg.ScatterPlotItem(size=10, pen=pg.mkPen(None), brush=pg.mkBrush(255, 255, 255, 120))
+      pos = np.random.normal(size=(2,n), scale=1e-5)
+      spots = [{'pos': pos[:,i], 'data': 1} for i in range(n)] + [{'pos': [0,0], 'data': 1}]
+      s1.addPoints(spots)
+      w1.addItem(s1)
+        
+      
+
+
+#    def Time(self):
+#        self.ui.lcdTime.display(strftime("%H"+":"+"%M"+":"+"%S"))
 
     def Test(self):
-        for x in range(0, 20):
+
+        for x in range(0, 10):
             print("Time %d" % (x))
             time.sleep(1)
             QtGui.qApp.processEvents()
